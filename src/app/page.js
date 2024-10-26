cc483e042dde5f4250599f05163cfbb78f13a245
Aurélie Solier <solier.lili@gmail.com>
2024-10-26 23:06:01 +0200

    .nojekyll ???

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
       <p>Voici quelques projets que j'ai réalisés.</p>
       <p>Voici quelques projets que j'ai réalisés.</p>
       <p>Voici quelques projets que j'ai réalisés.</p>
-      <p>Voici quelques projets que j'ai réalisés.</p>
-      <p>Voici quelques projets que j'ai réalisés.</p>
+      <p className="font-extrabold">Voici quelques projets que j'ai réalisés.</p>
+      <p className="w-52 h-56 bg-red-700 flex justify-center items-center ">Voici quelques projets que j'ai réalisés.</p>
       <p>Voici quelques projets que j'ai réalisés.</p>
       <p>Voici quelques projets que j'ai réalisés.</p>
       <p className="text-amber-700">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
