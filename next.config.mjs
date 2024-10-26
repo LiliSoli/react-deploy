a2513823947c662d040a2aa776c2a0e4d2107c7a
Aurélie Solier <solier.lili@gmail.com>
2024-10-26 16:12:24 +0200

    config gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// next.config.mjs
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'react-deploy'; // Remplace par le nom de ton dépôt GitHub
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
